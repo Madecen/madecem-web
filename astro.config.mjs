8d8da6594e994b5709adea7fe90d0af57110e723
Marc <ipracdeveloper@email.com>
2026-01-18 20:42:52 +0100

    cambios en astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  // <--- AÑADE ESTO
   site: 'https://www.madecem.com',
+
+  // Configuración de optimización de imágenes
+  image: {
+    // Forzamos el uso de Sharp para que los 1800KiB bajen a menos de 100KiB
+    service: {
+      entrypoint: 'astro/assets/services/sharp'
+    }
+  },
 
   vite: {
     plugins: [tailwindcss()]
