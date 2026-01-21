aa00b997770adb3bb8334bcb333ab95f27d38b33
Marc <ipracdeveloper@email.com>
2026-01-21 10:19:45 +0100

    Reinicio total: Nueva arquitectura Madecem v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+
 import tailwindcss from '@tailwindcss/vite';
-import sitemap from '@astrojs/sitemap';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.madecem.com',
-
-  // Configuración de optimización de imágenes
-  image: {
-    // Forzamos el uso de Sharp para que los 1800KiB bajen a menos de 100KiB
-    service: {
-      entrypoint: 'astro/assets/services/sharp'
-    }
-  },
-
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [sitemap()]
+  integrations: [react()]
 });
