058d2b2b7ead4b712d7b6dfae756cd8f356cbc3d
Marc <ipracdeveloper@email.com>
2026-01-24 18:50:35 +0100

    configuracion de OG -- Card de compartir redes ---

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-// astro.config.mjs
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  // Tu dominio real para que WhatsApp encuentre la imagen
+  site: 'https://madecem.com', 
+
   integrations: [
-    tailwind(), 
-    react()
+    tailwind()
   ],
 });
